f0bf7d594837911cb92c137a329652c55b30c592
Jenhtc <htc.jennifer@gmail.com>
2019-09-19 16:28:30 +0200

    Done V1.0

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,15 +2,30 @@
 
 const maxDifficulty = 196;
 
-let level = 4;
-let wordToGuess = pickWord(level).word;
-let wordToGuessWA = "SERGE"; //withoutAccents(wordToGuess);
-let gameStarted = false;
+let level = 1;
+let wordToGuess = ""; //pickWord(level).word;
+let wordToGuessWA = ""; //withoutAccents(wordToGuess);
+/* let gameStarted = false; */
 let usedLetters = "";
-let finished = false;
+/* let finished = false; */
 let totalGuesses = 0;
 let falseGuesses = 0;
-let mask = createMask();
+let maxFalseGuesses = 7;
+
+highScore = localStorage.getItem("highScore");
+if (highScore == null) {
+  localStorage.setItem("highScore", "");
+  highScore = "";
+}
+startGame();
+
+document.getElementById("start-game").addEventListener("click", function() {
+  startGame();
+});
+document.getElementById("difficulte").addEventListener("change", function() {
+  level = parseInt(this.value);
+  startGame();
+});
 /* console.log(mask);
 while (!finished) {
   letterEntered = prompt("Encodez une lettre").toUpperCase();
@@ -36,7 +51,6 @@ function updateMask(letter) {
   do {
     index = wordToGuessWA.indexOf(letter, index + 1);
     if (index > -1) {
-      console.log("ici" + mask);
       mask = replaceAt(mask, index, letter);
       counter++;
     }
@@ -83,4 +97,104 @@ function pickWord(difficulty) {
   let index = Math.floor(Math.random() * (rangeMax - rangeMin) + rangeMin);
 
   return words[index];
+}
+
+function displayKeyboard() {
+  const elementKeyboard = document.getElementById("keyboard");
+  const alphabet = "AZERTYUIOPQSDFGHJKLMWXCVBN";
+  displayHighscore();
+  elementKeyboard.innerHTML = "";
+  document.getElementById("image-pendu").src = "assets/images/pendu1.png";
+  document.getElementById("mask").innerHTML = mask;
+  for (elem of alphabet) {
+    let span = document.createElement("span");
+    span.setAttribute("class", "active");
+    span.innerHTML = elem;
+    elementKeyboard.append(span);
+    span.addEventListener("click", function() {
+      this.className = "inactive";
+      let letter = this.innerHTML;
+      usedLetters = usedLetters + letter;
+      if (updateMask(letter)) {
+        document.getElementById("mask").innerHTML = mask;
+      } else {
+        falseGuesses++;
+        document.getElementById("image-pendu").src =
+          "assets/images/pendu" + (falseGuesses + 1) + ".png";
+        document.getElementById("remaining-guesses").innerHTML =
+          "Nombre de fautes autorisées : " + (maxFalseGuesses - falseGuesses);
+        if (falseGuesses == maxFalseGuesses) {
+          alert("You lost ! The word was " + wordToGuess);
+          document.getElementById("mask").innerHTML = wordToGuess;
+        }
+      }
+      if (mask == wordToGuessWA) {
+        let score = calculScore();
+        addHighScore(score);
+        alert(
+          "Congrats ! The word was " +
+            wordToGuess.toUpperCase() +
+            ". You won " +
+            score +
+            " points. Click on RESTART GAME to play again!"
+        );
+      }
+    });
+  }
+}
+function addHighScore(score) {
+  let today = new Date();
+  console.log(today);
+  let year = today.getFullYear();
+  let month = today.getMonth();
+  let day = today.getDate();
+  let fullDate = day + "/" + month + "/" + year;
+  let newHighScore = fullDate + " : " + score;
+  if (highScore == "0") {
+    highScore = newHighScore;
+  } else {
+    highScore = newHighScore + ";" + highScore;
+  }
+  localStorage.setItem("highScore", highScore);
+  displayHighscore();
+}
+function displayHighscore() {
+  if (highScore == "0") return false;
+  let individualHighScores = highScore.split(";");
+  let tableHighScores = [];
+  individualHighScores.forEach(element => {
+    let x = element.split(":");
+    tableHighScores.push({ date: x[0].trim(), score: parseInt(x[1].trim()) });
+  });
+  tableHighScores.sort((a, b) => (a.score < b.score ? 1 : -1));
+  console.log(tableHighScores);
+  let html = '<ul class="list-group">';
+  tableHighScores.forEach(element => {
+    console.log(element);
+    html +=
+      '<li class="list-group-item">' +
+      element.date +
+      " : " +
+      element.score +
+      " pts</li>";
+  });
+  html += "</ul>";
+  document.getElementById("high-scores").innerHTML = html;
+}
+function calculScore() {
+  let score;
+  score = mask.length * level + (maxFalseGuesses - falseGuesses) * level;
+  return score;
+}
+function startGame() {
+  wordToGuess = pickWord(level).word;
+  wordToGuessWA = withoutAccents(wordToGuess);
+  usedLetters = "";
+  totalGuesses = 0;
+  falseGuesses = 0;
+  mask = createMask();
+  displayKeyboard();
+  console.log(mask);
+  document.getElementById("remaining-guesses").innerHTML =
+    "Nombre de fautes autorisées : " + (maxFalseGuesses - falseGuesses);
 }
